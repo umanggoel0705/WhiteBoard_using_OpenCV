14bc63467273265d21ea266abd78fb480920c0fc
umanggoel0705 <129640795+umanggoel0705@users.noreply.github.com>
2024-06-22 14:27:44 +0530

    Add files via upload

--- whiteboard.py
+++ whiteboard.py
@@ -22,11 +22,17 @@ prev_y = 0
 
 while cv2.waitKey(1) != 27:
     _, img = cap.read()
+    flag, display_img = cap.read()
 
-    if not _:
+    if not _ or not flag:
         break
+
     img = cv2.resize(img, (screen_width, screen_height))
     img = cv2.flip(img, 1)
+    
+    display_img = cv2.resize(display_img, (480, 360))
+    display_img = cv2.flip(display_img, 1)
+    
     res = mp_hands.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     if res.multi_hand_landmarks:
         for hand_landmark in res.multi_hand_landmarks:
@@ -46,13 +52,17 @@ while cv2.waitKey(1) != 27:
                         if(prev_x == 0 and prev_y == 0):
                             cv2.circle(output_img, (x,y), 2, (0,255,0))
                         else:
-                            cv2.line(output_img, (prev_x, prev_y), (x,y), (0,255,0), 5)
+                            cv2.line(output_img, (prev_x, prev_y), (x,y), (0,255,0), 5, cv2.LINE_AA)
                     prev_x = x
                     prev_y = y
                     pyautogui.moveTo(x+10,y+30)
-
-                # if id == 12:
-                #     cv2.circle(img, (x,y), 3, (255,0,0), 10)
+                if id == 12:
+                    cv2.circle(img, (x,y), 3, (255,0,0), 10)
+                    middle_x = x
+                    middle_y = y
+                    if abs(index_y - middle_y) < 60 and abs(index_x - middle_x) < 60:
+                        cv2.circle(output_img, (index_x, index_y), 10, (255,255,255), 20)
                         
     cv2.imshow("WhiteBoard", img)
-    cv2.imshow("Output", output_img)
+    cv2.imshow("Input", display_img)
+    cv2.imshow("WhiteBoard", output_img)
